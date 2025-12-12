fab093991a706677d6e8ce311c4e13791cb9be17
joel cobacho sole <cobachosolejoel@alumnes.ilerna.com>
2025-12-12 16:50:17 +0100

    Ejercició 1.1 Acabado
    hacemos que el hijo vaya mas rapido

--- cFilPrincipal/cFilPrincipal.java
+++ cFilPrincipal/cFilPrincipal.java
@@ -5,40 +5,42 @@ import cFil . cFil;
 public class cFilPrincipal {
 
   public static void main (String [] pArguments) {
+	  System.out.println("Fil principal iniciat.");
+	    System.out.println("Fil secundari iniciat.");
 
-    System . out . println ("Fil principal iniciat.");
-    System . out . println ("Fil secundari iniciat.");
+	    cFil vObjecteFil = new cFil("#1");
 
-    cFil vObjecteFil = new cFil ("#1");
+	    //alternativa: innecessari
+	    Thread vFil = new Thread(vObjecteFil);
 
-    //alternativa: innecessari
-    Thread vFil = new Thread (vObjecteFil);
+	    // Modificación 1.1 → proceso hijo muy rápido
+	    vObjecteFil.sTemporitzacio(100);
 
-    //alternativa: vObjecteFil
-    vFil . start ();
+	    //alternativa: vObjecteFil
+	    vFil.start();
 
-    System . out . println ("Iniciant execució procés principal");
+	    System.out.println("Iniciant execució procés principal");
 
-    try {
+	    try {
 
-      for (int vComptador = 0; vComptador < 10; vComptador ++) {
+	      for (int vComptador = 0; vComptador < 10; vComptador++) {
 
-        Thread . sleep (500);
+	        Thread.sleep(500);
 
-        System . out . println ("Despertant aturada " + vComptador + " procès principal");
+	        System.out.println("Despertant aturada " + vComptador + " procès principal");
 
-      }
+	      }
 
-    }
+	    }
 
-    catch (InterruptedException pExcepcio) {
+	    catch (InterruptedException pExcepcio) {
 
-      System . out . println ("Interrompent execució procès principal");
+	      System.out.println("Interrompent execució procès principal");
 
-    }
+	    }
 
-    System . out . println ("Acabant execució procès principal");
+	    System.out.println("Acabant execució procès principal");
 
-  }
+	  }
 
-}
+	}
