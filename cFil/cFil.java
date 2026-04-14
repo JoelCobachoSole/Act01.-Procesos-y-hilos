bd86139cdd16b119e08bd5df4305ef609615a555
Dani Garcia <danielgarciamartin6@alumnes.ilerna.com>
2026-04-14 11:00:54 +0200

    Update cFil.java

--- cFil/cFil.java
+++ cFil/cFil.java
@@ -1,56 +1,69 @@
-package cFil;
+package cFil;   // Indica el paquete al que pertenece la clase
 
-//alternativa: extends Thread
+// Alternativa a "extends Thread":
+// aquí usamos "implements Runnable" para que la clase pueda ejecutarse como hilo
 public class cFil implements Runnable {
 
-  String aNomFil;
+    // Nombre identificativo del hilo/proceso
+    String aNomFil;
 
-  int aTemporitzacio;
+    // Tiempo de espera entre ejecuciones (en milisegundos)
+    int aTemporitzacio;
 
-  public cFil (String pNomFil) {
+    // Constructor: recibe el nombre del hilo
+    public cFil(String pNomFil) {
 
-    aTemporitzacio = 500;
+        // Tiempo por defecto = 500 ms
+        aTemporitzacio = 500;
 
-    aNomFil = pNomFil;
-
-  }
-
-  public String gNomFil () {
-
-    return aNomFil;
-
-  }
-
-  public void sTemporitzacio (int pTemporitzacio) {
-
-    aTemporitzacio = pTemporitzacio;
-
-  }
-
-  public void run () {
-
-    System . out . println ("Iniciant execució procés " + aNomFil);
-
-    try {
-
-      for (int vComptador = 0; vComptador < 10; vComptador ++) {
-
-        Thread . sleep (aTemporitzacio);
-
-        System . out . println ("Despertant aturada " + vComptador + " procès " + aNomFil);
-
-      }
-
+        // Guarda el nombre recibido
+        aNomFil = pNomFil;
     }
 
-    catch (InterruptedException pExcepcio) {
-
-      System . out . println ("Interrompent execució procès " + aNomFil);
-
+    // Getter: devuelve el nombre del hilo
+    public String gNomFil() {
+        return aNomFil;
     }
 
-    System . out . println ("Acabant execució procès " + aNomFil);
+    // Setter: cambia el tiempo de espera
+    public void sTemporitzacio(int pTemporitzacio) {
+        aTemporitzacio = pTemporitzacio;
+    }
 
-  }
+    // Método que ejecutará el hilo
+    // Es obligatorio al implementar Runnable
+    public void run() {
 
+        // Mensaje inicial
+        System.out.println("Iniciant execució procés " + aNomFil);
+
+        try {
+
+            // Bucle que se repite 10 veces
+            for (int vComptador = 0; vComptador < 10; vComptador++) {
+
+                // Pausa el hilo durante el tiempo indicado
+                Thread.sleep(aTemporitzacio);
+
+                // Muestra el número de iteración
+                System.out.println(
+                    "Despertant aturada " + vComptador +
+                    " procès " + aNomFil
+                );
+            }
+
+        }
+        // Captura la excepción si el hilo es interrumpido
+        catch (InterruptedException pExcepcio) {
+
+            System.out.println(
+                "Interrompent execució procès " + aNomFil
+            );
+        }
+
+        // Mensaje final al terminar
+        System.out.println(
+            "Acabant execució procès " + aNomFil
+        );
+    }
 }
